ba1b3a7b09d89bd526b5ec1761510697039e2750
Himanshu <59023960+Himanshu-807@users.noreply.github.com>
2022-11-10 23:13:24 +0530

    multi module

---
+++ pet-clinic-data/src/main/java/himanshu/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package himanshu.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ pet-clinic-data/src/main/java/himanshu/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package himanshu.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
